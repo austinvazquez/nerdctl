7ac61830581dc8d9d83d0f16ad81deef3ee222d7
Austin Vazquez <macedonv@amazon.com>
2024-10-14 15:37:02 +0000

    Refactor load from archive as image util package
    Signed-off-by: Austin Vazquez <macedonv@amazon.com>

--- pkg/cmd/image/load.go
+++ pkg/cmd/image/load.go
@@ -18,95 +18,14 @@ package image
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"io"
-	"os"
 
 	containerd "github.com/containerd/containerd/v2/client"
-	"github.com/containerd/containerd/v2/core/images"
-	"github.com/containerd/containerd/v2/core/images/archive"
-	"github.com/containerd/containerd/v2/pkg/archive/compression"
-	"github.com/containerd/platforms"
 
 	"github.com/containerd/nerdctl/v2/pkg/api/types"
-	"github.com/containerd/nerdctl/v2/pkg/imgutil"
-	"github.com/containerd/nerdctl/v2/pkg/platformutil"
+	"github.com/containerd/nerdctl/v2/pkg/imgutil/load"
 )
 
-type readCounter struct {
-	io.Reader
-	N int
-}
-
-func (r *readCounter) Read(p []byte) (int, error) {
-	n, err := r.Reader.Read(p)
-	if n > 0 {
-		r.N += n
-	}
-	return n, err
-}
-
 func Load(ctx context.Context, client *containerd.Client, options types.ImageLoadOptions) error {
-	if options.Input != "" {
-		f, err := os.Open(options.Input)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		options.Stdin = f
-	} else {
-		// check if stdin is empty.
-		stdinStat, err := os.Stdin.Stat()
-		if err != nil {
-			return err
-		}
-		if stdinStat.Size() == 0 && (stdinStat.Mode()&os.ModeNamedPipe) == 0 {
-			return errors.New("stdin is empty and input flag is not specified")
-		}
-	}
-	decompressor, err := compression.DecompressStream(options.Stdin)
-	if err != nil {
-		return err
-	}
-	platMC, err := platformutil.NewMatchComparer(options.AllPlatforms, options.Platform)
-	if err != nil {
-		return err
-	}
-	return loadImage(ctx, client, decompressor, platMC, options)
-}
-
-func loadImage(ctx context.Context, client *containerd.Client, in io.Reader, platMC platforms.MatchComparer, options types.ImageLoadOptions) error {
-	// In addition to passing WithImagePlatform() to client.Import(), we also need to pass WithDefaultPlatform() to NewClient().
-	// Otherwise unpacking may fail.
-	r := &readCounter{Reader: in}
-	imgs, err := client.Import(ctx, r, containerd.WithDigestRef(archive.DigestTranslator(options.GOptions.Snapshotter)), containerd.WithSkipDigestRef(func(name string) bool { return name != "" }), containerd.WithImportPlatform(platMC))
-	if err != nil {
-		if r.N == 0 {
-			// Avoid confusing "unrecognized image format"
-			return errors.New("no image was built")
-		}
-		if errors.Is(err, images.ErrEmptyWalk) {
-			err = fmt.Errorf("%w (Hint: set `--platform=PLATFORM` or `--all-platforms`)", err)
-		}
-		return err
-	}
-	for _, img := range imgs {
-		image := containerd.NewImageWithPlatform(client, img, platMC)
-
-		// TODO: Show unpack status
-		if !options.Quiet {
-			fmt.Fprintf(options.Stdout, "unpacking %s (%s)...\n", img.Name, img.Target.Digest)
-		}
-		err = image.Unpack(ctx, options.GOptions.Snapshotter)
-		if err != nil {
-			return err
-		}
-
-		// Loaded message is shown even when quiet.
-		repo, tag := imgutil.ParseRepoTag(img.Name)
-		fmt.Fprintf(options.Stdout, "Loaded image: %s:%s\n", repo, tag)
-	}
-
-	return nil
+	_, err := load.FromArchive(ctx, client, options)
+	return err
 }
